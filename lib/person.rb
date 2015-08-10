dfc138a9e097cf5a5087eddce0f25b65878ab356
George Toothman <george.toothman@gmail.com>
2015-08-10 18:05:42 -0400

    finish assignment from lecture review

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,30 @@
+require 'pry'
 class Person
-  # code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+  def initialize(attributes)
+    # @name = attributes[:name]
+    # @birthday = attributes[:birthday]
+    # go through each attribute and set each instance variable named after 
+    # the key to the corresponding value
+
+    attributes.each do |property, value|
+      # instance_variable_set("@#{property}", value)
+      # self.name = "Avi"
+      # self.send("name=", "Avi")
+      self.send("#{property}=", value)
+    end
+  end
+
+  define_method "howdy" do
+    puts "Howdy!"
+  end
 end
+
+# .send("takes a string")
+# .send("rev" + "erse")
+# 1.send("+", 1)      
+# self.send("name=", "Avi")
+# seld.name=("Avi")
+
+# lines 21 and 22 are the same
+# interpolated symbol drops the colon
